74d7475d363c9c68ecb7f40b50eba775d4e08670
nyong0903 <nyongbramasta@gmail.com>
2024-11-13 19:27:22 +0700

    Create class_produk.php

---
+++ controllers/class_produk.php
@@ -0,0 +1,27 @@
+<?php
+    class produk {
+    private $dbh;
+    public function __construct($dbh){
+        $this->dbh = $dbh;
+    }
+
+    public function dataBarang(){
+        $sql="SELECT * FROM barang";
+        $rs = $this->dbh->query($sql);
+        return $rs;
+    }
+
+    public function dataProduk(){
+        $sql="SELECT * FROM produk";
+        $rs = $this->dbh->query($sql);
+        return $rs;
+    }
+    
+    public function getAllJenis(){
+        $sql = "SELECT * FROM jenis_produk";
+        // fungsi query, eksekusi query dan ambil datanya
+        $rs = $this->dbh->query($sql); 
+        return $rs;
+    }
+}
+ ?>
